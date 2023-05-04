21ca75f2efa56729a88e0705dd43723e7c133517
Vigo Lockhart <mdemeestere@tudelft.nl>
2023-05-04 15:35:46 +0200

    added rx to DS

--- Data_Sender.vhdl
+++ Data_Sender.vhdl
@@ -2,6 +2,27 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
+entity out_buff is
+  port (
+		regin : in    std_logic_vector(7 downto 0);
+		regout : out    std_logic_vector(7 downto 0)
+  );
+end entity;
+
+architecture Behavioral of out_buff is
+  
+begin
+  process(regin)
+  begin
+    regout <= regin;
+  end process;
+end architecture;
+
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
 entity Data_Sender is
   port (
         clk : in std_logic;
@@ -25,10 +46,8 @@ end entity;
 architecture behavioral of Data_Sender is
     component out_buff is
 		port (
-			clk: in std_logic;
-      en : in std_logic;
-			regin : in    std_logic;
-			regout : out    std_logic
+			regin : in    std_logic_vector(7 downto 0);
+		  regout : out    std_logic_vector(7 downto 0)
 		);
 	end component;
 
@@ -43,114 +62,157 @@ architecture behavioral of Data_Sender is
     tx_wait_ack_mine,
     tx_wait_ack_cross
   );
+
+  type reciever_state is (
+    rx_idle,
+    rx_read,
+    rx_toBuff
+  );
+
   signal ack_send : std_logic;
   signal ack_rec : std_logic;
   signal tx_state, tx_new : transmitter_state;
+  signal rx_state, rx_new : reciever_state;
   signal count_cross : std_logic;
-  signal count_ack : std_logic_vector(12 downto 0); 
+  signal count_ack : unsigned(12 downto 0); 
+  signal data : std_logic_vector(7 downto 0);
 begin
 
   -- assign states (clk process)
   process(clk)
   begin
     if(rising_edge(clk)) then
-      if(reset = '1') then
-        tx_new <= tx_idle;
-      end if;
       tx_state <= tx_new;
+      rx_state <= rx_new;
     end if;
   end process;
 
   -- tx state comb
-  process(tx_state, DS_in_cross, DS_in_mine, ack_send)
+  process(tx_state, DS_in_cross, DS_in_mine, ack_send, reset)
   begin
-    case tx_state is
-      when tx_idle =>
-        if(rising_edge(DS_in_cross)) then
-          tx_new <= tx_hold_cross;
-        elsif(rising_edge(DS_in_mine)) then
-          tx_new <= tx_hold_mine;     
-        elsif(ack_send = '1') then
-          tx_new <= tx_hold_ack;
-        else
-          tx_new <= tx_idle;
-        end if;
-
-      when tx_hold_mine =>
-        if(buffer_empty = '0') then
-          tx_new <= tx_mine;
-        else
-          tx_new <= tx_hold_mine;
-        end if;
-
-      when tx_hold_cross =>
-        if (count_cross = '1') then
-          count_cross <= '0';
-          if(buffer_empty = '0') then
-            tx_new <= tx_cross;
-          else
+    if (reset = '1') then
+      tx_new <= tx_idle;
+    else 
+      case tx_state is
+        when tx_idle =>
+          if(rising_edge(DS_in_cross)) then
             tx_new <= tx_hold_cross;
-          end if;
-        else 
-          count_cross <= '1';
-          tx_new <= tx_idle;
-        end if;
-
-      when tx_hold_ack =>
-        if(buffer_empty = '0') then
-          tx_new <= tx_ack;
-        else
-          tx_new <= tx_hold_ack;
-        end if;
-
-      when tx_mine =>
-        DS_out_UART_in <= "01000000";
-        write <= '1';
-        tx_new <= tx_wait_ack_mine;
-
-      when tx_cross =>
-        DS_out_UART_in <= "10000000";
-        write <= '1';
-        tx_new <= tx_wait_ack_cross;
-
-      when tx_ack =>
-        DS_out_UART_in <= "00010000";
-        write <= '1';
-        tx_new <= tx_idle;
-
-      when tx_wait_ack_mine =>
-        write <= '0';
-        if(count_ack < 4000)then
-          tx_new <= tx_wait_ack_mine;
-          count_ack <= count_ack + 1;
-        elsif (count_ack >= 4000) then
-          tx_new <= tx_hold_mine;
-        else
-          count_ack <= 0;
-          tx_new <= tx_wait_ack_mine;
-        end if;
-
-      when tx_wait_ack_cross =>
-        write <= '0';
-        if(count_ack < 4000)then
-          if(ack_rec = '1') then
-            count_ack <= '0';
-            tx_new <= tx_idle;
+          elsif(rising_edge(DS_in_mine)) then
+            tx_new <= tx_hold_mine;     
+          elsif(ack_send = '1') then
+            tx_new <= tx_hold_ack;
           else
-            tx_new <= tx_wait_ack_cross;
-            count_ack <= count_ack + 1;
+            tx_new <= tx_idle;
           end if;
-        elsif (count_ack >= 4000) then
-          tx_new <= tx_hold_cross;
-        else
-          count_ack <= 0;
+
+        when tx_hold_mine =>
+          if(buffer_empty = '0') then
+            tx_new <= tx_mine;
+          else
+            tx_new <= tx_hold_mine;
+          end if;
+
+        when tx_hold_cross =>
+          if (count_cross = '1') then
+            count_cross <= '0';
+            if(buffer_empty = '0') then
+              tx_new <= tx_cross;
+            else
+              tx_new <= tx_hold_cross;
+            end if;
+          else 
+            count_cross <= '1';
+            tx_new <= tx_idle;
+          end if;
+
+        when tx_hold_ack =>
+          if(buffer_empty = '0') then
+            tx_new <= tx_ack;
+          else
+            tx_new <= tx_hold_ack;
+          end if;
+
+        when tx_mine =>
+          DS_out_UART_in <= "01000000";
+          write <= '1';
+          tx_new <= tx_wait_ack_mine;
+
+        when tx_cross =>
+          DS_out_UART_in <= "10000000";
+          write <= '1';
           tx_new <= tx_wait_ack_cross;
-        end if;
 
-        when others =>
+        when tx_ack =>
+          DS_out_UART_in <= "00010000";
+          write <= '1';
           tx_new <= tx_idle;
+          ack_send <= '0';
 
-    end case;
+        when tx_wait_ack_mine =>
+          write <= '0';
+          if(count_ack = 4000)then
+            tx_new <= tx_wait_ack_mine;
+            count_ack <= count_ack + 1;
+          elsif (count_ack = 4000) then
+            tx_new <= tx_hold_mine;
+          else
+            count_ack <= "0";
+            tx_new <= tx_wait_ack_mine;
+          end if;
+
+        when tx_wait_ack_cross =>
+          write <= '0';
+          if(count_ack = 4000)then
+            if(ack_rec = '1') then
+              count_ack <= "0";
+              ack_rec <= '0';
+              tx_new <= tx_idle;
+            else
+              tx_new <= tx_wait_ack_cross;
+              count_ack <= count_ack + 1;
+            end if;
+          elsif (count_ack = 4000) then
+            tx_new <= tx_hold_cross;
+          else
+            count_ack <= "0";
+            tx_new <= tx_wait_ack_cross;
+          end if;
+
+          when others =>
+            tx_new <= tx_idle;
+
+      end case;
+    end if;
+
   end process;
 
+  -- RX combinatorial
+
+  process(rx_state, data_ready)
+  begin
+    if (reset = '1') then
+      rx_new <= rx_idle;
+    else 
+      case rx_state is
+        when rx_idle =>
+          if (data_ready = '1') then
+            rx_new <= rx_read;
+          else 
+            rx_new <= rx_idle;
+          end if;
+        when rx_read =>
+          if (DS_in_UART_out = "00010000") then
+            ack_rec <= '1';
+            rx_new <= rx_idle;
+          else
+            rx_new <= rx_toBuff;
+          end if;
+        when rx_toBuff =>
+          data <= DS_in_UART_out;
+          read <= '1';
+      end case;
+    end if;
+  end process;
+
+  REG: out_buff port map(regin => data, regout => DS_out);
 end architecture;
